a8f5364759115b3097913c596a56dc3477420e2c
yahiaoui.fatma <fatma.yahiaoui@esprit.tn>
2022-11-03 14:43:25 +0100

    test

---
+++ src/test/java/tn/esprit/rh/achat/operateur_junit.java
@@ -0,0 +1,14 @@
+package tn.esprit.rh.achat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class operateur_junit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
